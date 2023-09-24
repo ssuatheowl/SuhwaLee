90ab37c441589642b8622381f567525985336f98
ssuatheowl <35113814+ssuatheowl@users.noreply.github.com>
2023-09-24 13:11:48 +0200

    Java script update

--- app.js
+++ app.js
@@ -4,22 +4,26 @@ $(document).ready(function(){
 
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
 
       // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 400, function(){
+      var offset = $(hash).offset();
+      if (offset != undefined) {
+        // Prevent default anchor click behavior
+        event.preventDefault();
 
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 400, function(){
+
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    }
   });
 });
 
@@ -32,3 +36,41 @@ function hamburgerMenu() {
     x.className = "menu-ul";
   }
 }
+
+//$(document).ready(function(){
+  // Add smooth scrolling to all links
+  //$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    //if (this.hash !== "") {
+
+
+      // Store hash
+      //var hash = this.hash;
+
+      //var offset = $(hash).offset();
+      //console.log(offset)
+      //if (offset != 0)
+      //{
+
+
+      //var top = $(hash).offset().top
+
+      //if (top != 0) {
+        // Prevent default anchor click behavior
+      //event.preventDefault();
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      //$('html, body').animate({
+      //  scrollTop: top
+      //}, 400, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+      //  window.location.hash = hash;
+      //});
+   // }
+ // }
+   // } // End if
+ // });
+//});
